726d65870b1db8537471e25a66d38e437425615c
jorge alberto canul vivas <lunac51@hotmail.com>
2016-02-26 17:40:34 -0500

    Proyecto-LasKekas

---
+++ frontend/views/site/_form.php
@@ -0,0 +1,30 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model common\models\Profile */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="profile-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $model->user_id = $user->getPrimaryKey() ?>
+
+    <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'last_name')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'gender')->dropDownList([1 => 'Hombre', 2 => 'Mujer']) ?>
+
+
+    <div class="form-group">
+        <?= Html::submitButton($model->isNewRecord ? Yii::t('app', 'Create') : Yii::t('app', 'Update'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
